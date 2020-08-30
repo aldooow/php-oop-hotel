a935fd0c55989f6e7ba397925301b8751815b6ca
Aldo Cahuana <aldooow@icloud.com>
2020-08-30 23:33:04 +0200

    Work in progress!!

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php
 require_once(__DIR__ . '/stanza.php');
-
-  $stanze = [
-    [
-      'room_number' => '100',
-      'floor' => '1',
-      'beds' => '2',
-      'created_at' => '2018-05-28 13:33:11',
-      'updated_at' => '2018-05-28 13:33:11'
-    ],
-    ['id' => '2','room_number' => '101','floor' => '1','beds' => '3','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '3','room_number' => '102','floor' => '1','beds' => '3','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '4','room_number' => '103','floor' => '1','beds' => '1','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '5','room_number' => '104','floor' => '1','beds' => '2','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '6','room_number' => '105','floor' => '1','beds' => '2','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '7','room_number' => '201','floor' => '2','beds' => '2','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '8','room_number' => '202','floor' => '2','beds' => '3','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '9','room_number' => '203','floor' => '2','beds' => '3','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '10','room_number' => '204','floor' => '2','beds' => '1','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '11','room_number' => '205','floor' => '2','beds' => '2','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '12','room_number' => '301','floor' => '3','beds' => '3','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '13','room_number' => '302','floor' => '3','beds' => '3','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '14','room_number' => '303','floor' => '3','beds' => '2','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11'],
-    ['id' => '15','room_number' => '304','floor' => '3','beds' => '1','created_at' => '2018-05-28 13:33:11','updated_at' => '2018-05-28 13:33:11']
-    ];
+require_once(__DIR__ . '/db.php');
 ?>
 
 <!DOCTYPE html>
@@ -47,7 +24,7 @@ require_once(__DIR__ . '/stanza.php');
       <li>Created at: <?php echo $room_data['created_at'] ?></li>
       <li>Updated at: <?php echo $room_data['updated_at'] ?></li>
     </ul>
-    
+
     <?php
     };
     ?>
